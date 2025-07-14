499bac92283202b4a36041daeb4c892d1bcc698a
Chandra6388 <cppatel6388@gmail.com>
2025-07-15 00:45:06 +0530

    done

--- frontend1/src/App.tsx
+++ frontend1/src/App.tsx
@@ -14,53 +14,55 @@ import AdminRoute from "@/routes/adminRoutes";
 import UserRoute from "@/routes/userRoutes";
 import { useAuth } from '@/contexts/AuthContext';
 import { useEffect } from "react";
-// import { useNavigation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 
 const queryClient = new QueryClient();
 
-const App = () => {
-  // const navigate = useNavigation();
+
+const AppRoutes = () => {
+  const navigate = useNavigate();
   const { isAuthenticated, user } = useAuth();
-  console.log("isAuthenticated:", isAuthenticated);
-
-
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     navigate('/login');
-  //   }
-  // }, [isAuthenticated, navigate]);
-
-  console.log("user:", user?.role);
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login');
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
+      <Toaster />
+      <Sonner />
+      <Routes>
+        {isAuthenticated ? (
+          <>
+            <Route path="/user/*" element={<UserRoute />} />
+            <Route path="/admin/*" element={<AdminRoute />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+          </>
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
 
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
       <CartProvider>
         <TooltipProvider>
           <BrowserRouter>
-            <Toaster />
-            <Sonner />
-            <Routes>
-              {isAuthenticated ? (
-                <>
-                  <Route path="/user/*" element={<UserRoute />} />
-                  <Route path="/admin/*" element={<AdminRoute />} />
-                </>
-              ) : (
-                <>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/forgot-password" element={<ForgotPassword />} />
-                </>
-              )}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <AppRoutes />
           </BrowserRouter>
         </TooltipProvider>
       </CartProvider>
-
     </QueryClientProvider>
   );
 };
