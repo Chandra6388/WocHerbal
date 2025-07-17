67a0c93c771750eb4f09fcdcf61cd469a8c02b08
Chandra6388 <cppatel6388@gmail.com>
2025-07-17 23:45:19 +0530

    done

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -94,6 +94,8 @@ exports.getUserProfile = async (req, res, next) => {
   }
 };
 
+
+
 exports.updatePassword = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id).select('+password');
