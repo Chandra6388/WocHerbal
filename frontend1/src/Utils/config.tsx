11be5b0ea27ac21f5994d6e80c28171e5497689a
ajay90989 <ajay.office.patidar@gmail.com>
2025-07-22 20:56:02 +0530

    øk

--- frontend1/src/Utils/config.tsx
+++ frontend1/src/Utils/config.tsx
@@ -1,3 +1,5 @@
 export const API_URL = 'http://localhost:5000/api';
 // export const API_URL = "http://82.29.166.155:5000/api";
 // export const API_URL = "https://wocherbal.com/backend/api";
+
+// export const API_URL = `${window.location.origin}/backend/api`;
